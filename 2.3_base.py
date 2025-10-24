fa68f5d16e1b9b70eb247a923be8395d3d7317ce
Lina Tumanyan <tumanyan_lina@mail.ru>
2025-10-25 01:07:24 +0300

    Number of vowels fixed

--- 2.3_base.py
+++ 2.3_base.py
@@ -1,8 +1,15 @@
 text = input("Введите строку для подсчёта гласных: ")
-count = 0
 
-for char in text.lower():
-    if char in 'уеэёоаыяиюeyioa':
-        count += 1
-
-print("Количество гласных:", count)
+if text == "":
+    print("Ошибка: пустая строка.")
+else:
+    for char in text:
+        if char in "0123456789":
+            print("Ошибка: нельзя вводить цифры.")
+            break
+    else:
+        count = 0
+        for char in text.lower():
+            if char in "аеёиоуыэюяaeiou":
+                count += 1
+        print("Количество гласных:", count)

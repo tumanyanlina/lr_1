666b0fa4fc8490837c19037379749672071d48f8
Lina Tumanyan <tumanyan_lina@mail.ru>
2025-10-25 00:10:57 +0300

    Factorial

---
+++ 2.1_base.py
@@ -0,0 +1,11 @@
+n = float(input("Введите число для поиска факториала: "))
+
+
+def get_factorial(n):
+    factorial = 1
+    for i in range(1, int(n) + 1):
+        factorial = factorial * i
+    return factorial
+
+
+print(get_factorial(n))

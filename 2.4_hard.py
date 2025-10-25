7c372fc87b423d69d32e15f7d69abaa3e06392b2
Lina Tumanyan <tumanyan_lina@mail.ru>
2025-10-25 09:32:09 +0300

    Added new lines

--- 2.4_hard.py
+++ 2.4_hard.py
@@ -3,11 +3,14 @@ print("Я загадал число от 1 до 50. Угадайте его")
 n = random.randint(1, 50)
 
 while True:
-    guess = int(input("Ваша догадка: "))
-    if guess < n:
-        print("Загаданное число больше.")
-    elif guess > n:
-        print("Загаданное число меньше.")
-    else:
-        print("Ура! Вы угадали!")
-        break
+    try:
+        guess = int(input("Ваша догадка: "))
+        if guess < n:
+            print("Загаданное число больше.")
+        elif guess > n:
+            print("Загаданное число меньше.")
+        else:
+            print("Ура! Вы угадали!")
+            break
+    except ValueError:
+        print("Пожалуйста, вводите только числа!")

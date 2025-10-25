926593b7ea4036e5286c34f7ad99e9ac1211dcb5
Lina Tumanyan <tumanyan_lina@mail.ru>
2025-10-25 09:52:57 +0300

    The Counter

---
+++ 2.5_hard.py
@@ -0,0 +1,19 @@
+def word_counter(file_name: str):
+    with open(file_name, 'r', encoding="utf-8") as f:
+        content = f.read()
+
+    for char in "(),.!?;:-—\n":
+        content = content.replace(char, " ")
+
+    content = content.lower()
+    words = content.split()
+    counter = {}
+
+    for word in words:
+        counter[word] = counter.get(word, 0) + 1
+
+    return counter
+
+
+file_path = r'c:\Users\tuman\OneDrive\Desktop\МиТП_лабы\python\file.txt'
+print("Частота слов в файле:", word_counter(file_path))

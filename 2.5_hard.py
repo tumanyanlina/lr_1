916849579179121d9a1b1bdf50323dc9cb39b83a
Lina Tumanyan <tumanyan_lina@mail.ru>
2025-10-25 09:58:09 +0300

    Changed the Counter

--- 2.5_hard.py
+++ 2.5_hard.py
@@ -2,11 +2,15 @@ def word_counter(file_name: str):
     with open(file_name, 'r', encoding="utf-8") as f:
         content = f.read()
 
+    if not content.strip():
+        return "Файл пуст"
+
     for char in "(),.!?;:-—\n":
         content = content.replace(char, " ")
 
     content = content.lower()
     words = content.split()
+
     counter = {}
 
     for word in words:
@@ -16,4 +20,5 @@ def word_counter(file_name: str):
 
 
 file_path = r'c:\Users\tuman\OneDrive\Desktop\МиТП_лабы\python\file.txt'
+
 print("Частота слов в файле:", word_counter(file_path))
